b1532741767217f2140f3684debf97b6396afb14
javafalcon <javafalcon@163.com>
2018-04-30 20:10:14 +0800

    add files

--- program/establishBenchmarkDataset.py
+++ program/establishBenchmarkDataset.py
@@ -22,7 +22,9 @@ def establishBenchmarkDataset(ws, datafile, savefile):
     # load PDNA-224-PSSM.mat
     # datafile = 'PDNA-224-PSSM.mat'
     pssm = sio.loadmat(datafile)
-
+    excludes={'__header__', '__version__','__globals__'}
+    for key in excludes:
+        del(pssm[key])
     # read fasta file
     #fastafile = 'PDNA-224.fasta'
     #seq_records = SeqIO.parse(fastafile)
@@ -70,13 +72,69 @@ def establishBenchmarkDataset(ws, datafile, savefile):
     sio.savemat(savefile,dataset)
 
 
-def establishBinPSSM(datafile, savefile):
+def establishBenchmarkDatasetwithSlipWindons(ws, datafile, savefile):
+    X = []
+    Y = []
+    k = 0
+    t = 0
     # load PDNA-224-PSSM.mat
     # datafile = 'PDNA-224-PSSM.mat'
     pssm = sio.loadmat(datafile)
     excludes={'__header__', '__version__','__globals__'}
     for key in excludes:
         del(pssm[key])
+    # read fasta file
+    #fastafile = 'PDNA-224.fasta'
+    #seq_records = SeqIO.parse(fastafile)
+
+    # build slip window with size 11*2+1=23
+    
+    # read 'PDNA-224-binding-sites.txt'
+    with open('../data/PDNA-224-binding-sites.txt', 'r') as pbsreader:
+        for line in pbsreader:
+            line = line.strip()
+            if '>' in line:
+                sid = line[1:]
+                p = pssm[sid]
+                seqlen = len(p)
+                col = np.size(p,1)
+                for j in range(seqlen):
+                    #create a array
+                    d = np.ndarray(shape=(ws*2+1,col),dtype=np.int8)
+                    
+                    if j < ws:
+                        d[0:ws-j] = p[j-ws:]
+                        d[ws-j:2*ws+1] = p[0: ws+j+1]
+                    elif j > seqlen - ws -1:
+                        d[0:ws] = p[j-ws:j]
+                        d[ws:ws + seqlen -j] = p[j:]
+                        d[ws+seqlen-j:] = p[0:ws-seqlen+j+1]
+                    else:
+                        d[::]=p[j-ws:j+ws+1]
+                
+                    X.insert(k,d)
+                    Y.append([1,0])
+                    k += 1
+                    #print('k={},t={}'.format(k,t))
+            else:
+                sites = line.split()
+                for s in sites:
+                    idx = eval(s)
+                    #print('t={},idx={}'.format(t,idx))
+                    #print(t+idx-1)
+                    Y[t + idx -1] = [0,1]
+                t=k        
+    #save benchmark data set
+    dataset={}
+    dataset['data']=X
+    dataset['target'] = Y
+    sio.savemat(savefile,dataset)
+
+
+def establishBinPSSM(datafile, savefile):
+    # load PDNA-224-PSSM.mat
+    # datafile = 'PDNA-224-PSSM.mat'
+    pssm = sio.loadmat(datafile)
     
     for sid in pssm:
         if sid not in ['__header__', '__version__','__globals__']:
@@ -103,5 +161,5 @@ def establishBinPSSM(datafile, savefile):
     #save benchmark data set
     sio.savemat(savefile,pssm)
 
-establishBinPSSM('../data/PDNA-224-PSSM.mat','../data/PDNA-224-PSSM-bin.mat')    
-#establishBenchmarkDataset(11,'../data/PDNA-224-PSSM-bin.mat','../data/PDNA-224-PSSM-bin-11.mat')                
+#establishBinPSSM('../data/PDNA-224-PSSM.mat','../data/PDNA-224-PSSM-bin.mat')    
+establishBenchmarkDatasetwithSlipWindons(11,'../data/PDNA-224-PSSM-bin.mat','../data/PDNA-224-PSSM-bin-11.mat')                
